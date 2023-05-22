51955bbf6d40108b1a3fa5758a4e462221766369
Alexandr KhramoV <it.khramov@gmail.com>
2023-05-22 20:05:47 +0300

    Done

---
+++ hwpy_2_2_3_2.py
@@ -0,0 +1,13 @@
+print('Ведите стадии развития человека по порядку на латыни.')
+homo_list = ['Homo habilis',
+             'Homo erectus',
+             'Homo ergaster',
+             'Homo antecessor',
+             'Homo neanderthalensis']
+a1 = input('Первая стадия: ')
+a2 = input('Вторая стадия: ')
+a3 = input('Третья стадия: ')
+a4 = input('Четвертая стадия: ')
+a5 = input('Пятая стадия: ')
+print('Вы считаете, что стадии развития человека проходили в таком порядке')
+print(a1, a2, a3, a4, a5, sep="=>")
